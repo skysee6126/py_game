b6b9e303f6486f0a4a8c699537f44433298b0a99
skysee6126 <chooshinhye@gmail.com>
2021-04-25 15:51:31 +0900

    Avoiding game

--- pygame_basic/game.py
+++ pygame_basic/game.py
@@ -5,13 +5,16 @@ screen_width = 640
 screen_height = 480
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-pygame.display.set_caption("Test game")
+pygame.display.set_caption("Avoiding game")
 
 #FPS
 clock = pygame.time.Clock()
 
-# background = pygame.image.load("C:\Users\케이지케이\Documents\practice\game\pygame_basic\background.jpg")
-character = pygame.image.load("https://www.flaticon.com/svg/vstatic/svg/3885/3885025.svg?token=exp=1619081421~hmac=9f4bd262dddec45be649b11af322333e")
+#background image
+background = pygame.image.load("background.jpg")
+
+#character
+character = pygame.image.load("character.jpg")
 character_size = character.get_rect().size
 character_width = character_size[0]
 character_height = character_size[1]
@@ -20,16 +23,16 @@ character_y_pos = screen_height - character_height
 
 #Move
 to_x = 0
-to_y = 0
-
 character_speed = 0.6
 
+#enemy
 enemy = pygame.image.load("enemy.jpg")
 enemy_size = enemy.get_rect().size
 enemy_width = enemy_size[0]
 enemy_height = enemy_size[1]
-enemy_x_pos = (screen_width/2) - (enemy_width/2)
-enemy_y_pos = screen_height - enemy_height
+enemy_x_pos = 0
+enemy_y_pos = 0
+enemy_speed = 10
 
 
 game_font = pygame.font.Font(None, 40)
@@ -49,18 +52,13 @@ while running:
         to_x -= character_speed
       elif event.key == pygame.K_RIGHT:
         to_x += character_speed
-      elif event.key == pygame.K_UP:
-        to_y -= character_speed
-      elif event.key == pygame.K_DOWN:
-        to_y += character_speed
+
     if event.type == pygame.KEYUP:
       if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
         to_x = 0
-      elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-        to_y = 0
 
+#position of character
 character_x_pos += to_x *dt
-character_y_pos += to_y *dt
 
 #가로
 if character_x_pos < 0:
@@ -68,11 +66,8 @@ if character_x_pos < 0:
 elif character_x_pos > screen_width - character_width:
   character_x_pos = screen_width - character_width
 
-#세로
-if character_y_pos < 0:
-  character_y_pos = 0
-elif character_y_pos > screen_height - character_height:
-  character_y_pos = screen_height - character_height
+#position of enemy
+enemy_y_pos += enemy_speed
 
 
 #collision
@@ -88,8 +83,9 @@ if character_rect.colliderect(enemy_rect):
   print("Bump!")
   running = False
 
-  screen.fill((0,0,130))
-  # screen.blit(background, (0,0))
+  
+
+  screen.blit(background, (0,0))
   screen.blit(character, (character_x_pos,character_y_pos))
   screen.blit(enemy, (enemy_x_pos,enemy_y_pos))
 
