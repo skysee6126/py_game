44860765e47454af639488fc95a83fa41ebc4eb6
skysee6126 <chooshinhye@gmail.com>
2021-04-27 16:40:06 +0900

    ball event update

--- pygame_basic/game3.py
+++ pygame_basic/game3.py
@@ -36,12 +36,33 @@ weapon_size = weapon.get_rect().size
 weapon_width = weapon_size[0]
 
 weapons = []
+weapon_speed = 10
 
+#Making 4 balls 
+ball_images = [
+  pygame.image.load(os.path.join(image_path, "ball.png")),
+  pygame.image.load(os.path.join(image_path, "ball2.png")),
+  pygame.image.load(os.path.join(image_path, "ball3.png")),
+  pygame.image.load(os.path.join(image_path, "ball4.png"))
+]
 
-# game_font = pygame.font.Font(None, 40)
+#inital speed of balls
+ball_speed_y = [-18, -15, -12, -9]
 
-# total_time = 10
-# start_ticks = pygame.time.get_ticks()
+balls = []
+
+#frist happening ball
+balls.append({
+  "pos_x": 50,
+  "pos_y": 50,
+  "img_idx": 0, #index of ball image
+  "to_x": 3, #direction of x if there is - value: left, + value: right
+  "to_y": 3,
+  "init_spd_y": ball_speed_y #inital speed
+})
+
+weapon_to_remove = -1
+ball_to_remove = -1
 
 #Even roof
 running = True
@@ -73,31 +94,93 @@ if character_x_pos < 0:
 elif character_x_pos > screen_width - character_width:
   character_x_pos = screen_width - character_width
 
+#Weapons
 weapons = [[w[0], w[1] - weapon_speed] for w in weapons]
 weapons = [[w[0], w[1]] for w in weapons if w[1]>0]
 
+#Ball position
+for ball_idx, ball_val in enumerate(balls):
+  ball_pos_x = ball_val["pos_x"]
+  ball_pos_y = ball_val["pos_y"]
+  ball_img_idx = ball_val["img_idx"]
 
-# #collision
-# character_rect = character.get_rect()
-# character_rect.left = character_x_pos
-# character_rect.right = character_y_pos
+  ball_size = ball_images[ball_img_idx].get_rect().size
+  ball_width = ball_size[0]
+  ball_height = ball_size[1]
 
-# enemy_rect = enemy.get_rect()
-# enemy_rect.left = enemy_x_pos
-# enemy_rect.right = enemy_y_pos
+  #positin change for spring against wall
+  if ball_pos_x < 0 or ball_pos_x > screen_width - ball_width:
+    ball_val["to_x"] = ball_val["to_x"]* -1
 
-# if character_rect.colliderect(enemy_rect):
-#   print("Bump!")
-#   running = False
+  #height position
+  if ball_pos_y >= screen_height - stage_height - ball_height:
+    ball_val["to_y"] = ball_val["init_spd_y"]
+  else:
+    ball_val["to_y"] += 0.5
+  
+  ball_val["pos_x"] += ball_val["pos_x"]
+  ball_val["pos_y"] += ball_val["pos_y"]
 
-  screen.blit(background, (0,0))
 
-  for weapon_x_pos, weapon_y_pos in weapons:
-    screen.blit(weapon, (weapon_x_pos,weapon_y_pos))
 
-  screen.blit(stage, (0,screen_height - stage_height))
-  screen.blit(character, (character_x_pos,character_y_pos))
+#collision
+character_rect = character.get_rect()
+character_rect.left = character_x_pos
+character_rect.top = character_y_pos
 
-  pygame.display.update()
+for ball_idx, ball_val in enumerate(balls):
+  ball_pos_x = ball_val["pos_x"]
+  ball_pos_y = ball_val["pos_y"]
+  ball_img_idx = ball_val["img_idx"]
+
+  ball_rect = ball_images[ball_img_idx].get_rect()
+  ball_rect.left = ball_pos_x
+  ball_rect.top = ball_pos_y
+
+#collision between character - ball
+if character_rect.colliderect(ball_rect):
+  print("Bump!")
+  running = False
+  break
+
+#collision between weapon - ball
+  for weapon_idx, weapon_val in enumerate(weapons):
+    weapon_pos_x = weapon_val[0]
+    weapon_pos_y = weapon_val[1]
+
+    weapon_rect = weapon.get_rect()
+    weapon_rect.left = weapon_pos_x
+    weapon_rect.top = weapon_pos_y
+
+  if weapon_rect.collidedict(ball_rect):
+    weapon_to_remove = weapon_idx
+    ball_to_remove = ball_idx
+    break
+
+if ball_to_remove > -1:
+  del balls[ball_to_remove]
+  ball_to_remove = -1
+
+if weapon_to_remove > -1:
+  del weapons[weapon_to_remove]
+  weapon_to_remove = -1
+
+
+
+screen.blit(background, (0,0))
+
+for weapon_x_pos, weapon_y_pos in weapons:
+  screen.blit(weapon, (weapon_x_pos,weapon_y_pos))
+
+for inx, val in enumerate(balls):
+  ball_pos_x = val["pos_x"]
+  ball_pos_y = val["pos_y"]
+  ball_img_idx = val["img_idx"]
+  screen.blit(ball_images[ball_img_idx], (ball_pos_x, ball_pos_y))
+
+screen.blit(stage, (0,screen_height - stage_height))
+screen.blit(character, (character_x_pos,character_y_pos))
+
+pygame.display.update()
 
 pygame.quit()
